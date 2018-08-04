59a6fbbaf69d9cbef10ca5e9a5d06b03c5c4e95b
Calvin Wong <kalokwong9@gmail.com>
2018-08-04 19:32:59 +0800

    Update playground

--- demo.playground/Contents.swift
+++ demo.playground/Contents.swift
@@ -1,6 +1,18 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import PlaygroundSupport
 
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+if let url = URL(string: "https://api.myjson.com/bins/yfua8") {
+    URLSession.shared.dataTask(with: url) { data, response, error in
+        if let data = data {
+            if let jsonString = String(data: data, encoding: .utf8) {
+                print(jsonString)
+            }
+        }
+    }.resume()
+}
 
 
