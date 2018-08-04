f9d0acfc7721a5047b74ac235f30125896446c21
Calvin Wong <kalokwong9@gmail.com>
2018-08-04 21:23:47 +0800

    Add JSON decode

--- demo.playground/Contents.swift
+++ demo.playground/Contents.swift
@@ -15,4 +15,20 @@ if let url = URL(string: "https://api.myjson.com/bins/yfua8") {
     }.resume()
 }
 
+struct Response: Codable { // or Decodable
+    let foo: String
+}
+
+if let url = URL(string: "https://api.myjson.com/bins/yfua8") {
+    URLSession.shared.dataTask(with: url) { data, response, error in
+        if let data = data {
+            do {
+                let res = try JSONDecoder().decode(Response.self, from: data)
+                print(res.foo)                
+            } catch let error {
+                print(error)
+            }
+        }
+    }.resume()
+}
 
