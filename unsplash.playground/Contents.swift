5f99be51c4bef5dda83ef6ca8cdb59058b6662ee
Calvin Wong <kalokwong9@gmail.com>
2018-08-18 22:33:00 +0800

    Add unsplash api

--- unsplash.playground/Contents.swift
+++ unsplash.playground/Contents.swift
@@ -4,3 +4,29 @@ import UIKit
 import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
+
+extension URL {
+    private static var baseUrl: String {
+        return "https://api.unsplash.com/"
+    }
+    
+    static func with(string: String) -> URL? {
+        return URL(string: "\(baseUrl)\(string)")
+    }
+}
+
+// retrive a random photo
+
+if let url = URL.with(string: "photos/random") {
+    var urlRequest = URLRequest(url: url)
+    urlRequest.setValue("Client-ID YOUR_ACCESS_KEY", forHTTPHeaderField: "Authorization")
+    
+    URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+        if let data = data {
+            if let jsonString = String(data: data, encoding: .utf8) {
+                print(jsonString)
+            }
+        }
+    }.resume()
+}
+
