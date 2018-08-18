562674a83b3353861f2dcb6b38035174543648e3
Calvin Wong <kalokwong9@gmail.com>
2018-08-19 01:44:53 +0800

    Update to get image urls

--- unsplash.playground/Contents.swift
+++ unsplash.playground/Contents.swift
@@ -17,16 +17,35 @@ extension URL {
 
 // retrive a random photo
 
+struct URLs: Decodable {
+    let raw: String
+    let full: String
+    let regular: String
+    let small: String
+    let thumb: String
+}
+
+struct Image: Decodable {
+    let id: String
+    let width: Int
+    let height: Int
+    let likes: Int
+    let color: String
+    let urls: URLs
+}
+
 if let url = URL.with(string: "photos/random") {
     var urlRequest = URLRequest(url: url)
     urlRequest.setValue("Client-ID YOUR_ACCESS_KEY", forHTTPHeaderField: "Authorization")
     
     URLSession.shared.dataTask(with: urlRequest) { data, response, error in
         if let data = data {
-            if let jsonString = String(data: data, encoding: .utf8) {
-                print(jsonString)
+            do {
+                let image = try JSONDecoder().decode(Image.self, from: data)
+                print(image)
+            } catch let error {
+                print(error)
             }
         }
     }.resume()
 }
-
